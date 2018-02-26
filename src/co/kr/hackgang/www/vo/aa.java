676d38975e2f448db1c6fd813c0d2db4518a1869
myh2 <36657359+myh2@users.noreply.github.com>
2018-02-26 15:31:12 +0900

    ㅇㅇㅇㅇ

---
+++ src/co/kr/hackgang/www/vo/aa.java
@@ -0,0 +1,20 @@
+package co.kr.hackgang.www.vo;
+
+public class aa {
+	String s;
+	int a;
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	
+}
